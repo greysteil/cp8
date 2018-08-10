b0bfbc3e6bc638b6a3eb4f060032cb038b793d7a
miles <miles@thespecials.com>
2018-08-10 16:28:25 +0100

    label test

--- lib/issue_delegator.rb
+++ lib/issue_delegator.rb
@@ -7,7 +7,7 @@ class IssueDelegator
 
   def run
     if label.name.start_with?(LABEL_PREFIX)
-      require 'pry'; binding.pry
+
     end
   end
 
---
+++ test/label_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class LabelTest < Minitest::Test
+  def test_detecting_prefix
+    label = Label.new("move-to: elsewhere")
+
+    assert_equal "move-to", label.prefix
+  end
+
+  def test_detecting_suffix
+    label = Label.new("move-to: elsewhere")
+
+    assert_equal "elsewhere", label.suffix
+  end
+end
